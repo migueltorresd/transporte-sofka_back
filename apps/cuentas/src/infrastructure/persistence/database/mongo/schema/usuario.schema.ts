8184d26471475d49b5e7fe0419d8e0236f771652
migueltorresd <migueltorresdiaz1992@gmail.com>
2023-04-23 21:12:28 -0500

    Titulo: se  crean archivos de config para mongo descripcion: se crean archivos de config para mongo en los dos microservicios

---
+++ apps/cuentas/src/infrastructure/persistence/database/mongo/schema/usuario.schema.ts
@@ -0,0 +1,35 @@
+import { Prop, Schema } from "@nestjs/mongoose";
+import { UsuarioDomainEntity } from "apps/cuentas/src/domain";
+import { SchemaTypes } from "mongoose";
+import { version } from "os";
+
+@Schema({ collection: 'usuarios',versionKey: false, strict: false })
+export class UsuarioSchema extends UsuarioDomainEntity{
+    @Prop({type: SchemaTypes.ObjectId,
+        auto: true, })
+    id: string;
+
+    @Prop({ required: false})
+    nombreUsuario: string;
+
+    @Prop({ required: true})
+    password: string;
+
+    @Prop({ required: true})
+    nombres: string;
+
+    @Prop({ required: true})
+    apellidos: string;
+
+    @Prop({ required: false})
+    dni: string;
+
+    @Prop({ required: true})
+    correo: string;
+
+    @Prop({ required: false})
+    telefono: string;
+
+    @Prop({ required: true})
+    esConductor: boolean;
+}
---
+++ apps/cuentas/src/infrastructure/persistence/database/mongo/schema/vehiculo.schema.ts
@@ -0,0 +1,30 @@
+@Schema({ collection: 'vehiculos',versionKey: false, strict: false })
+export class VehiculoSchema extends VehiculoDomainEntity{
+    @Prop({type: SchemaTypes.ObjectId,
+        auto: true, })
+    id: string;
+
+    @Prop({ required: true})
+    conductorId: string;
+
+    @Prop({ required: true})
+    marca: string;
+
+    @Prop({ required: true})
+    modelo: string;
+
+    @Prop({ required: true})
+    tipo: string;
+
+    @Prop({ required: true})
+    placa: string;
+
+    @Prop({ required: true})
+    color: string;
+
+    @Prop({ required: true})
+    capacidad: number;
+
+    @Prop({ required: true})
+    disponible: boolean;
+}
