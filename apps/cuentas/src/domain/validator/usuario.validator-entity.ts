c72b1be3855066fcb5ba1b3edd8f6a7ffaad7599
Diego Sánchez <clyzer.dev@gmail.com>
2023-04-26 07:04:26 -0300

    rol validator change

--- apps/cuentas/src/domain/validator/usuario.validator-entity.ts
+++ apps/cuentas/src/domain/validator/usuario.validator-entity.ts
@@ -12,7 +12,8 @@ export function validarUsuario(
     typeof entity.rol !== 'number' ||
     isNaN(entity.rol) ||
     entity.rol < 0 ||
-    !(entity.rol == RolTypes.USUARIO || RolTypes.CONDUCTOR)
+    entity.rol !== RolTypes.USUARIO.valueOf() ||
+    RolTypes.CONDUCTOR.valueOf()
   ) {
     throw new BadRequestException('El rol no es valido');
   }
